7cc76a0f67dd54457596c35281131289981373f5
adhl-0216 <hanlim2002@gmail.com>
2024-10-22 13:08:29 +0000

    optimized SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,34 @@ const cairo = Cairo({
   variable: '--font-cairo'
 });
 
-
 export const metadata: Metadata = {
-  title: "ADHL",
-  description: "Professional Portfolio of Adrian Han Lim Oah",
+  title: "ADHL - Adrian Han Lim Oah",
+  description: "Explore my professional portfolio showcasing skills, projects, and experiences in Software Development, Cybersecurity, and DevOps.",
+  keywords: "Adrian Han Lim Oah, portfolio, software development, cybersecurity, full-stack developer, projects, experience, devops",
   icons: {
-    icon: '/favicon.ico'
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: "ADHL - Adrian Han Lim Oah",
+    description: "Discover my professional portfolio. Explore projects, experiences, and skills in software development and cybersecurity.",
+    url: "https://adhl-0216.github.io",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Adrian Han Lim Oah Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://adhl-0216.github.io",
   }
 };
 
@@ -29,18 +51,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <html
       lang="en"
       className={`${museoModerno.variable} ${cairo.variable} scroll-smooth`}
     >
       <body className="flex flex-col font-cairo text-text-50 bg-primary-950">
-        <Header />
+        <header>
+          <Header />
+        </header>
         <main className="px-12 lg:px-32">
           {children}
         </main>
-        <Footer />
+        <footer>
+          <Footer />
+        </footer>
       </body>
     </html>
   );
